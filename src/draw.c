ae0fa519be333649b91dd5fe736fa3c0fb47ced2
rob <robin.navarro@etu.u-bordeaux.fr>
2017-11-18 12:27:50 +0100

    Ajout de commentaires dans la fonction drawGame pour la gestion du scroll

--- src/draw.c
+++ src/draw.c
@@ -4,50 +4,51 @@ void drawGame(Game *game)
 {
   clearScreen(game->screen);
   drawMap(game);
-
   SDL_RenderPresent(game->screen->pRenderer);
 }
 
 //Affichage de ce que l'on voit à l'écran seulement
 
 void drawMap(Game *game){
-    int perso_x = game->perso->x;
+    int perso_x = game->perso->x;       //Juste plus pratique
     int perso_y = game->perso->y;
-    int dep_x;
+    int dep_x;                          //écart entre début map et perso
     int dep_y;
-    static int save_dep;
-
-    if (perso_x > game->wmap*32 - WINDOW_W/2){
-        dep_x = save_dep;
-
-    } else if (perso_x > WINDOW_W/2){
-        dep_x = perso_x - WINDOW_W/2;   //On affiche avec le perso au centre
-        save_dep = dep_x;
+    static int save_dep;                //sauvegarde du déplacement X
+    
+    //DEP HORIZONTAL
+    if (perso_x > game->wmap*32 - WINDOW_W/2){      //quand perso en fin de map
+        dep_x = save_dep;                           //On utiliise la sauvegarde 
+    } else if (perso_x > WINDOW_W/2){               //Début du scroll
+        dep_x = perso_x - WINDOW_W/2;               //WINDOW_W/2 pour garder le perso au millieu
+        save_dep = dep_x;                           //Sauvegarde du dep
     } else {
         dep_x = 0;
     }
 
-    
+    //DEP VERTICAL
     if (perso_y > WINDOW_H){
         dep_y = perso_y - WINDOW_H/2;
-    } /*else if (perso_y < 0){  //Très experimentale
-        dep_y = perso_y + WINDOW_H/2;
-    } */else {
+    } else {
         dep_y = 0;
     }
 
-
-    for (int x = 0; x < WINDOW_W/32 + 1; x++){      //ATTENTION AU +1 dangereux source de crash
-        if (x + dep_x/32>= game->wmap){
+    //Les +1 dans les boucles sont pour afficher le début de laprocaine ligne à l'écran
+    for (int x = 0; x < WINDOW_W/32 + 1; x++){
+        if (x + dep_x/32>= game->wmap){         //Pour éviter les segfault à cause du +1
             break;
         }
         for (int y = 0; y < WINDOW_H/32 + 1; y++){
-            if (y + dep_y/32>= game->hmap){
+            if (y + dep_y/32>= game->hmap){     //Même "astuce"
                 break;
             }
             switch (game->map[x + dep_x/32][y + dep_y/32]->type){
                 case GROUND:
-                    drawImage(game->map[x + dep_x/32][y + dep_y/32]->image, x*32 - dep_x%32, y*32 - dep_y%32, game->screen->pRenderer);
+                    drawImage(game->map[x + dep_x/32][y + dep_y/32]->image,
+                                x*32 - dep_x%32,
+                                y*32 - dep_y%32,
+                                game->screen->pRenderer
+                    );
                   break;
             }
         }
